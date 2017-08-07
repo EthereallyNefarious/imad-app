668f52381f11da826394caf548b88815ff38ef91
EthereallyNefarious <30668148+EthereallyNefarious@users.noreply.github.com>
2017-08-08 00:08:25 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ app.get('/:articleName', function (req, res) {
     var articleName = req.parans.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
