50359ff6e9ed5fbc4caee66dd047ded6c91b10b7
EthereallyNefarious <30668148+EthereallyNefarious@users.noreply.github.com>
2017-08-08 00:39:06 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 // Move the Image
 var img= document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft='100px';
-
+function moveRight () {
+    var marginLeft=0;
+    marginLeft =  marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
 }
+img.onclick = function() {
+    var interval = setInterval(moveRight,100);
+};
